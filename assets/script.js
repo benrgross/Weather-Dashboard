ce863ee7d04115c4793d1afd5a6aace8f91b3f97
Ben Gross <benrgross@gmail.com>
2021-01-24 14:48:58 -0500

    pseudo coding build out of html.

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+// ----- SIDE NAV  ------
+
+// -- connect to #search button with click/submit event
+
+// -- connect to form and save text in variable chosenCity
+
+//---- click event appends chosenCity  #city-ul
+
+// ----- BUILD ELEMENT FOR CITY LIST AND APPEND -----
+
+// BUILD -- > <div class="col-sm-12 col-md-12 col-lg-12">
+
+// <div class="card" id="city-list" style="width: auto">
+//   <ul class="list-group list-group-flush" id="city-ul">
+//   </ul>
+// </div>
+
+//  Append to #search-col
+
+// BUILD list items:
+// <li class="list-group-item">chosenCity</li>
+
+// APPEND to #city-ul
+
+//---- CURRENT WEATHER INFO -------
+
+// when city is submitted -- onlick submit event from search bar - > retrieve current weather info
+
+//----- CREATE/BUILD/APPEND WEATHER DATA INTO WEATHER-BOX -----
+
+// BUILD -- > <h5 class="card-title"> current date (current weather icon)</h5>
+
+// APPEND--> .card-body
+
+//  build
+
+// DATA
+
+// user interactions
+
+// functions
