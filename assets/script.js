a0dc7279b38b8defcfd095986327f230e3a8c729
Ben Gross <benrgross@gmail.com>
2021-01-25 16:56:55 -0500

    connected openwether api made  request.

--- assets/script.js
+++ assets/script.js
@@ -52,3 +52,25 @@
 
 // a for each loop that matches card number to day number ?
 // all data come in a variable together or have to insert individual data
+
+//My api key
+var APIKey = "b833b868df96016cabebdb7a4ca15977";
+
+// Here we are building the URL we need to query the database
+var queryURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" +
+  APIKey;
+
+// We then created an AJAX call
+$.ajax({
+  url: queryURL,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+  // Create CODE HERE to Log the queryURL
+  // Create CODE HERE to log the resulting object
+  // Create CODE HERE to calculate the temperature (converted from Kelvin)
+  // Create CODE HERE to transfer content to HTML
+  // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
+  // Create CODE HERE to dump the temperature content into HTML
+});
